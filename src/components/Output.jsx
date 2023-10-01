8e1191b0e5485d7fc61ce515b9c8e3519368cf8b
Saungyati <saungyati576@gmail.com>
2023-10-01 17:29:05 +0630

    updated-height

--- src/components/Output.jsx
+++ src/components/Output.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Output = ({output}) => {
   return (
-    <div className=' w-1/3 bg-black text-white text-right text-xl h-10'>
+    <div className=' w-1/3 bg-black text-white text-right text-xl h-11'>
       {output}
     </div>
   )
